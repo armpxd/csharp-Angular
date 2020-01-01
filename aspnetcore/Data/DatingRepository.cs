152c4def30b1a5392013aebe6c9ced29f784c0e3
Alvaro Martínez <armpmartinez@gmail.com>
2020-01-01 17:40:44 -0400

    commit

--- aspnetcore/Data/DatingRepository.cs
+++ aspnetcore/Data/DatingRepository.cs
@@ -47,7 +47,6 @@ namespace aspnetcore.Data
         public async Task<User> GetUser(int id)
         {
             var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
-            
             return user;
         }
 
